7dc56a919f02a4ba8a457dabd368986a01e4be9e
Ranko Liang <rankoliang@gmail.com>
2020-09-14 20:50:44 -0400

    add flights seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Airport.delete_all
-
 def airport_codes(n, codes = [])
   codes + ('AAA'..'ZZZ').to_a.sample(n)
 end
 
 airport_codes(15, %w[LGA SFO]).each do |code|
   Airport.create(code: code)
+end
+
+airports = Airport.all
+
+100.times.each do
+  origin, destination = airports.sample(2)
+  origin.departing_flights.create(destination: destination,
+                                  departure_time: Faker::Time.forward(days: 200),
+                                  duration: rand(60..360).minutes)
 end
