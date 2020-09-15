3cafeeaa3dbf38f642b025c5e19b31ebf88e6a5c
Ranko Liang <rankoliang@gmail.com>
2020-09-15 01:39:31 -0400

    add flight search form

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -6,4 +6,18 @@ class Flight < ApplicationRecord
 
   belongs_to :origin, class_name: 'Airport', foreign_key: 'origin_id'
   belongs_to :destination, class_name: 'Airport', foreign_key: 'destination_id'
+
+  scope :departure_dates, lambda {
+                            select(:departure_time)
+                              .where('departure_time > ?', DateTime.now)
+                              .order(:departure_time)
+                          }
+
+  def departure_date_formatted
+    departure_time.strftime('%m-%d-%Y')
+  end
+
+  def departure_date
+    departure_time.to_date
+  end
 end
