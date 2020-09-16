145ee9f3f0a38b8e5b53b9f15998ff8193ee477c
Ranko Liang <rankoliang@gmail.com>
2020-09-16 18:21:13 -0400

    Add create and show action for bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,27 @@
 class BookingsController < ApplicationController
   def new
     @booking = Booking.new(flight_id: params[:flight_id])
+    @passengers = Array.new(params[:passengers].to_i) { @booking.passengers.build }
+  end
+
+  def create
+    booking = Booking.new(booking_params)
+    if booking.save
+      redirect_to booking
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @booking = Booking.includes(:passengers).find(params[:id])
+    @flight = @booking.flight
+    @passengers = @booking.passengers
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: %i[name email])
   end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,6 +2,7 @@ class Booking < ApplicationRecord
   validates :flight, presence: true
 
   belongs_to :flight
-  has_many :booking_passengers
+  has_many :booking_passengers, inverse_of: :booking
   has_many :passengers, through: :booking_passengers
+  accepts_nested_attributes_for :booking_passengers, :passengers
 end
